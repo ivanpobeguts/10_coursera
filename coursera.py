8a9d2bd4bd0fc54f4fd74c517bb0b67e8cf7f74c
Pobeguts Ivan <Ivan.Pobeguts@aton.ru>
2018-06-19 13:15:08 +0300

    add lxml requirement, add getting courses list from xml

--- coursera.py
+++ coursera.py
@@ -1,7 +1,18 @@
+from lxml import etree
+import requests
 
 
 def get_courses_list():
-    pass
+    courses_list = []
+    courses_url = 'https://www.coursera.org/sitemap~www~courses.xml'
+    courses = requests.get(courses_url)
+    bytes_courses_list = bytes(courses.text, 'utf-8')
+    root = etree.fromstring(bytes_courses_list)
+    for url in root.getchildren():
+        for loc in url.getchildren():
+            if 'python' in loc.text:
+                courses_list.append(loc.text)
+    return courses_list
 
 
 def get_course_info(course_slug):
@@ -13,4 +24,4 @@ def output_courses_info_to_xlsx(filepath):
 
 
 if __name__ == '__main__':
-    pass
+    print(get_courses_list())
