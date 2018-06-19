48f95db84445146e61d24a2da5efe5ce8c6c98e5
Pobeguts Ivan <Ivan.Pobeguts@aton.ru>
2018-06-19 14:21:19 +0300

    add course page parsing

--- coursera.py
+++ coursera.py
@@ -1,5 +1,6 @@
 from lxml import etree
 import requests
+from bs4 import BeautifulSoup
 
 
 def get_courses_list():
@@ -15,13 +16,27 @@ def get_courses_list():
     return courses_list
 
 
-def get_course_info(course_slug):
-    pass
-
+def get_course_info(course_url):
+    course_info_dict = {}
+    course_html = requests.get(course_url)
+    soup = BeautifulSoup(course_html.text, 'html.parser')
+    course_info_dict['name'] = soup.find('h1', {"class": "title display-3-text"}).text
+    course_info_dict['language'] = soup.find('div', {"class": "rc-Language"}).text
+    rating = soup.find('div', {"class": "ratings-text bt3-visible-xs"})
+    if rating:
+        course_info_dict['rating'] = rating.text[:3]
+    else:
+        course_info_dict['rating'] = '-'
+    course_info_dict['start_date'] = soup.find('div', {"class": "startdate rc-StartDateString caption-text"}).text[7:]
+    course_info_dict['weeks'] = len(soup.findAll('div', {"class": "week"}))
+    return course_info_dict
 
 def output_courses_info_to_xlsx(filepath):
     pass
 
 
 if __name__ == '__main__':
-    print(get_courses_list())
+    courses_list = get_courses_list()
+    print(courses_list)
+    for course in courses_list:
+        print(get_course_info(course))
